10e0e27e2f7b876a42505c1f97771c2858b823f9
Manoel Vilela <manoel_vilela@engineer.com>
2017-01-11 23:25:56 -0300

    Add equivalences.lisp with equivalences rules
    :: rules
    * double-negation
    * de-morgan
    
    :: simplification
    * clean-double-negations

---
+++ equivalences.lisp
@@ -0,0 +1,53 @@
+;; Common Lisp Script
+;; Manoel Vilela
+
+;; set of equivalences transform for propositional calculus
+
+(load "parser.lisp")
+(load "operators.lisp")
+
+(defun double-negation (exp)
+  "Double negation equivalence rule ::
+   (~ (~ p)) <=> p"
+  (if (and (negationp exp)
+           (negationp (cadr exp)))
+      (cadadr exp)
+      exp))
+
+(defun clean-double-negations (exp)
+  "Search in the expression's tree for double-negations and apply
+   the equivalence of double-negation to simplify it."
+  (if (and (listp exp)
+           (find-if #'listp exp))
+      (let ((head (car exp))
+            (tail (cdr exp)))
+        (when (listp head)
+          (setq head (mapcar #'double-negation head)))
+        (cons head
+              (mapcar #'double-negation (clean-double-negations tail))))
+      exp))
+
+(defun de-morgan (exp)
+  "De Morgan equivalence rule ::
+  (~ (^ p q)) <=> (v (~ p) (~ q))
+  (^ p q) <=> (~ (v (~ p) (~ q)))
+  (~ (v p q)) <=> (^ (~ p) (~ q))
+  (v p q) <=> (~ (^ (~ p) (~ q)))"
+  (clean-double-negations
+   (cond ((and (negationp exp) ;; (~ (^ p q)) <=> (v (~ p) (~ q))
+               (conjunctionp (first-operand exp)))
+          (make-disjunction
+           (make-negation (first-operand (first-operand exp)))
+           (make-negation (second-operand (first-operand exp)))))
+         ((and (negationp exp) ;; (~ (v p q)) <=> (^ (~ p) (~ q))
+               (disjunctionp (first-operand exp)))
+          (make-conjunction
+           (make-negation (first-operand (first-operand exp)))
+           (make-negation (second-operand (first-operand exp)))))
+         ((conjunctionp exp) ;; (^ p q) <=> (~ (v (~ p) (~ q)))
+          (make-negation (make-disjunction (make-negation (first-operand exp))
+                                           (make-negation (second-operand exp)))))
+         ((disjunctionp exp) ;; (v p q) <=> (~ (^ (~ p) (~ q)))
+          (make-negation (make-conjunction (make-negation (first-operand exp))
+                                           (make-negation (second-operand exp)))))
+         (t exp))))
