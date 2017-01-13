35d657f213c8b7f92b40ba35888777ac2680b20f
Manoel Vilela <manoel_vilela@engineer.com>
2017-01-12 22:04:51 -0300

    Add new selectors and operation type checkers
    :: selectors
    * first-of-first-operand
    * first-of-second-operand
    * second-of-second-operand
    * second-of-first-operand
    
    :: operation type
    * unary-operationp => '(~)
    * binary-operationp => '(^ v => <=>)

--- parser.lisp
+++ parser.lisp
@@ -4,6 +4,14 @@
 ;; selectors
 (setf (symbol-function 'first-operand) #'cadr)
 (setf (symbol-function 'second-operand) #'caddr)
+(setf (symbol-function 'first-of-first-operand) #'(lambda (x) (first-operand
+                                                          (first-operand x))))
+(setf (symbol-function 'first-of-second-operand) #'(lambda (x) (first-operand
+                                                           (second-operand x))))
+(setf (symbol-function 'second-of-second-operand) #'(lambda (x) (second-operand
+                                                            (second-operand x))))
+(setf (symbol-function 'second-of-first-operand) #'(lambda (x) (second-operand
+                                                           (first-operand x))))
 (setf (symbol-function 'operator) #'car)
 (setf (symbol-function 'operands) #'cdr)
 
@@ -12,26 +20,37 @@
 (defun operationp (exp op)
   "Based a 'op that can be a symbol, verify if the list
    can be a operation of 'op"
+  (eq (operator exp) op))
+
+(defun unary-operationp (exp)
   (and (not (atom exp))
-       (eq (operator exp) op)
-       (not (null (first-operand exp)))))
+       (= (length (operands exp)) 1)))
+
+(defun binary-operationp (exp)
+  (and (not (atom exp))
+       (= (length (operands exp)) 2)))
 
 (defun negationp (exp)
   "Verify if the expression is a negation"
-  (operationp exp '~))
+  (and (unary-operationp exp)
+       (operationp exp '~)))
 
 (defun conjunctionp (exp)
   "Verify if the expression is a conjunction"
-  (operationp exp '^))
+  (and (binary-operationp exp)
+       (operationp exp '^)))
 
 (defun disjunctionp (exp)
   "Verify if the expression is a disjunction"
-  (operationp exp 'v))
+  (and (binary-operationp exp)
+       (operationp exp 'v)))
 
 (defun implicationp (exp)
   "Verify if the expression is an implication"
-  (operationp exp '=>))
+  (and (binary-operationp exp)
+       (operationp exp '=>)))
 
 (defun biconditionalp (exp)
   "Verify if the expression is a biconditional"
-  (operationp exp '<=>))
+  (and (binary-operationp exp)
+       (operationp exp '<=>)))
