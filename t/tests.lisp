ceab4bce1213ab90f81feb4b7fc7f32f3a75306a
Manoel Vilela <manoel_vilela@engineer.com>
2018-11-30 21:54:33 -0300

    Makes t/tests.lisp formatting more consistent

--- t/tests.lisp
+++ t/tests.lisp
@@ -3,26 +3,33 @@
 
 (plan nil)
 
-;; inference rule tests
+
 (diag "== Inference rules!")
+
 (is (modus-ponens '(^ (=> p q) p))
     'q
     "Inference: MODUS-PONENS")
+
 (is (modus-tollens '(^ (=> p q) (~ p)))
     '(~ q)
     "Inference: MODUS-TOLLENS")
+
 (is (syllogism-disjunctive '(^ (v p q) (~ p)))
     'q
     "Inference: SYLLOGISM-DISJUNCTIVE")
+
 (is (syllogism-hypothetical '(^ (=> x y) (=> y z)))
     '(=> X Z)
     "Inference: SYLLOGISM-HYPOTHETICAL")
+
 (is (addiction 'p 'q)
     '(v p q)
     "Inference: ADDICTION")
+
 (is (conjunction '(=> p q) 'p)
     '(^ (=> P Q) P)
     "Inference: CONJUNCTION")
+
 (is (absorption '(=> r (^ x y)))
     '(=> R (^ R (^ X Y)))
     "Inference: ABSORPTION")
@@ -33,8 +40,8 @@
     's
     "Inference: SIMPLIFICATION SECOND")
 
-(diag "== Equivalence rules!")
 
+(diag "== Equivalence rules!")
 
 (is (de-morgan '(^ p q))
     '(~ (v (~ p) (~ q)))
